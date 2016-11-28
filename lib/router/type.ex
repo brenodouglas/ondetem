e92f7935b862ba3b6b745ab92b7dba8569b965cd
Breno Douglas <breno.douglas@paieterno.com.br>
2016-11-28 14:52:58 -0200

    Crud API for type of locale

--- lib/router/type.ex
+++ lib/router/type.ex
@@ -10,7 +10,12 @@ defmodule OndeTem.Router.Type do
 
       desc "Retrieve a type of locales"
       get "/" do
-        query = from t in TypeModel, select: t.id
+        query = from t in TypeModel, select: %{id: t.id,
+                                              description: t.description,
+                                              code: t.code,
+                                              updated_at: t.updated_at,
+                                              created_at: t.inserted_at
+                                             }
         conn
         |> json(Repo.all(query))
       end
@@ -20,7 +25,12 @@ defmodule OndeTem.Router.Type do
         requires :id, type: Integer
       end
       get ":id" do
-        query = from t in TypeModel, select: t.id, where: t.id == ^params[:id]
+        query = from t in TypeModel, select: %{id: t.id,
+                                              description: t.description,
+                                              code: t.code,
+                                              updated_at: t.updated_at,
+                                              created_at: t.inserted_at
+                                             }, where: t.id == ^params[:id]
         conn
         |> json(Repo.one(query))
       end
@@ -34,11 +44,40 @@ defmodule OndeTem.Router.Type do
         changeset = TypeModel.changeset(%TypeModel{}, params)
         case Repo.insert(changeset) do
           {:ok, model} ->
-            conn |> json(model)
+            conn |> json(%{id: model.id, description: model.description, code: model.code, inserted_at: model.inserted_at})
           {:error, changeset} ->
-            conn |> json(%{error: changeset})
+            conn |> json(%{error: true})
         end
-        conn |> json(%{description: params[:description]})
+      end
+
+      desc "Delete a type of locale"
+      params do
+        requires :id, type: Integer
+      end
+      delete ":id" do
+        type = Repo.get!(TypeModel, params[:id])
+        case Repo.delete type do
+          {:ok, model} ->
+            conn |> json(%{id: model.id, description: model.description, code: model.code, inserted_at: model.inserted_at})
+          {:error, changeset} ->
+              conn |> json(%{error: true})
+        end
+      end
+
+      desc "Update a type of locale"
+      params do
+        requires :id, type: Integer
+      end
+      put ":id" do
+        type = Repo.get!(TypeModel, params[:id])
+        type = Ecto.Changeset.change type, params
+
+        case Repo.update type do
+          {:ok, model} ->
+            conn |> json(%{id: model.id, description: model.description, code: model.code, inserted_at: model.inserted_at})
+          {:error, changeset} ->
+              conn |> json(%{error: true})
+        end
       end
     end
   end
